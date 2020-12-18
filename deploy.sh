440cc71c1ae4a307d1e0256db7365fb48706f11e
abetomoki0420 <kimi.jack.of.all.trades@gmail.com>
2020-12-18 22:58:54 +0900

    fix(deploy.sh): fix more

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:suchwowinc/composition-api-tailwindcss-sample-1.git main:gh-pages
+git push -f git@github.com:suchwowinc/composition-api-tailwindcss-sample-1.git master:gh-pages
 
 cd -
