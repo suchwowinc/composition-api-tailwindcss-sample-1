e525a349d0e08ebd43ddf4ab49eddd535377dd12
abetomoki0420 <kimi.jack.of.all.trades@gmail.com>
2020-12-19 11:46:31 +0900

    build: add firebase configurations

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,52 @@
   <div class="w-9/12 m-auto">
     <p >{{ doge.age }}</p>
     <div class="flex justify-center">
-      <Button @doge="getDoge" @click="countUp">Click Me</Button>
+      <MyButton @doge="getDoge" @click="countUp">Click Me</MyButton>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 interface Doge {
   name: string;
   age: number;
 }
 
-
 import { ref, reactive, onMounted, computed } from "vue";
-import Button from "@/components/Button.vue";
+import MyButton from "@/components/MyButton.vue";
 
-const doge = reactive<Doge>({
-  name: 'bob',
-  age: 999
-})
+export default {
+  components:{
+    MyButton
+  },
+  setup(){
+    const doge = reactive<Doge>({
+      name: 'bob',
+      age: 999
+    })
 
-onMounted( () => {
-  doge.age = 100
-})
+    onMounted( () => {
+      doge.age = 100
+    })
 
-const countUp = () => {
-  doge.age++;
+    const countUp = () => {
+      doge.age++;
+    }
+
+    const getDoge = (v:any) => {
+      console.log(v);
+    }
+
+    return {
+      doge,
+      countUp,
+      getDoge
+    }
+  }
 }
 
-const getDoge = (v:any) => {
-  console.log(v);
-}
+
+
 
 
 </script>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <button @click="click" class="p-4 mt-60 border rounded-lg cursor-pointer hover:bg-blue-500 hover:text-white transition duration-300">
+    <slot/>
+  </button>
+</template>
