fefc0b727409f7489e11f3534372423ccae11183
abetomoki0420 <kimi.jack.of.all.trades@gmail.com>
2020-12-19 11:50:49 +0900

    fix: button color

--- src/App.vue
+++ src/App.vue
@@ -45,10 +45,5 @@ export default {
     }
   }
 }
-
-
-
-
-
 </script>
 
--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="click" class="p-4 mt-60 border rounded-lg cursor-pointer hover:bg-blue-500 hover:text-white transition duration-300">
+  <button @click="click" class="p-4 mt-60 border rounded-lg cursor-pointer hover:bg-red-500 hover:text-white transition duration-300">
     <slot/>
   </button>
 </template>
