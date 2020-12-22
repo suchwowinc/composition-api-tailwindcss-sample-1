fc53b7816f6ce029cb6300f9e070949ed8bd877c
abetomoki0420 <kimi.jack.of.all.trades@gmail.com>
2020-12-22 20:33:48 +0900

    chore: add over 9000

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,8 +1,10 @@
 <template>
-
   <div class="flex flex-col items-center w-9/12 m-auto mt-6">
     <div class=" text-gray-500">The AMAZING Counter</div>
     <div class="mt-10 text-9xl text-gray-500  p-6 rounded-xl bg-yellow-100">{{ doge.age }}</div>
+    <div class="mt-8">
+      <iframe v-if="over9000" width="560" height="315" src="https://www.youtube.com/embed/PCHxU7witPA?controls=0&amp;start=9" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen/>
+    </div>
     <div class="mt-10">
       <MyButton @doge="getDoge" @click="countUp(1)">count up</MyButton>
     </div>
@@ -11,6 +13,9 @@
     </div>
     <div class="mt-4">
       <MyButton @doge="getDoge" @click="countUp(100)">super count up</MyButton>
+    </div>
+    <div class="mt-4">
+      <MyButton @doge="getDoge" @click="countUp(1000)">incredible count up</MyButton>
     </div>
   </div>
 </template>
@@ -21,7 +26,7 @@ interface Doge {
   age: number;
 }
 
-import { reactive } from "vue";
+import { reactive, computed } from "vue";
 import MyButton from "@/components/MyButton.vue";
 import { useInformation } from "@/store/store";
 
@@ -39,17 +44,22 @@ export default {
 
     const countUp = (v:number) => {
       doge.age += v
-      setMessage(`You add count: ${v}`)
+      setMessage(`You add count: ${v} amounts:${ doge.age }`)
     }
 
     const getDoge = (v:any) => {
       console.log(v);
     }
 
+    const over9000 = computed( () => {
+      return doge.age >= 9000
+    })
+
     return {
       doge,
       countUp,
-      getDoge
+      getDoge,
+      over9000
     }
   }
 }
