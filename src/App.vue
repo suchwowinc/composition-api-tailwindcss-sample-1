296e9f8719642aca223bd0760a2ee4f15d89d289
tomo-abe <kimi.jack.of.all.trades@gmail.com>
2021-01-01 20:16:35 +0900

    fix: adjust header layout (#5)

--- src/App.vue
+++ src/App.vue
@@ -8,50 +8,50 @@
           >Composition API + Tailwind CSS sample</span
         >
       </div>
-      <div class="md:flex items-center justify-end md:flex-1 lg:w-0">
-        <a
-          href="#"
-          class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
-        >
-          <router-link to="/">Home</router-link>
-        </a>
-        <a
-          href="#"
-          class="ml-8 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
-        >
-          <router-link to="/list">List</router-link>
-        </a>
-        <a
-          href="#"
-          class="ml-8 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
-        >
-          <router-link to="/cart">Cart[{{ counts }}]</router-link>
-        </a>
-        <a
-          href="#"
-          class="ml-8 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
-        >
-          <router-link to="/modal">Modal</router-link>
-        </a>
-        <a
-          href="#"
-          class="ml-8 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
-        >
-          <router-link to="/mouse">Mouse</router-link>
-        </a>
-        <a
-          href="#"
-          class="ml-8 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
-        >
-          <router-link to="/chat">Chat</router-link>
-        </a>
-        <a
-          href="#"
-          class="ml-8 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
-        >
-          <router-link to="/video">Video</router-link>
-        </a>
-      </div>
+    </div>
+    <div class="flex py-4">
+      <a
+        href="#"
+        class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+      >
+        <router-link to="/">Home</router-link>
+      </a>
+      <a
+        href="#"
+        class="ml-8 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+      >
+        <router-link to="/list">List</router-link>
+      </a>
+      <a
+        href="#"
+        class="ml-8 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+      >
+        <router-link to="/cart">Cart[{{ counts }}]</router-link>
+      </a>
+      <a
+        href="#"
+        class="ml-8 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+      >
+        <router-link to="/modal">Modal</router-link>
+      </a>
+      <a
+        href="#"
+        class="ml-8 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+      >
+        <router-link to="/mouse">Mouse</router-link>
+      </a>
+      <a
+        href="#"
+        class="ml-8 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+      >
+        <router-link to="/chat">Chat</router-link>
+      </a>
+      <a
+        href="#"
+        class="ml-8 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+      >
+        <router-link to="/video">Video</router-link>
+      </a>
     </div>
   </div>
   <information-button />
